67016fdcdcfc77e01c9d6fcf364de70016fe2856
Ana Kniss <ackniss@gmail.com>
2022-01-17 22:32:45 -0300

    Added iterative exercises

---
+++ iterative_exercises/itera_example3.c
@@ -0,0 +1,112 @@
+/*#include <stdio.h>
+
+int main() {
+	float nota, nota1, i;
+
+	nota1 = 0;
+
+	for (i = 0; i < 3; i++) {
+		printf("Digite a nota: ");
+		scanf_s("%f", &nota);
+
+		if (nota > nota1){
+			nota1 = nota;
+
+		}
+	}
+
+	printf("A maior nota é: %f", nota1);
+
+}
+
+
+#include <stdio.h>
+
+int main() {
+	int i;
+	float nota1, nota2, nota3, media;
+
+	for (i = 0; i = 20; i++) {
+		printf("Digite a primeira nota: ");
+		scanf_s("%f", &nota1);
+		printf("Digite a segunda nota: ");
+		scanf_s("%f", &nota2);
+		printf("Digite a terceira nota: ");
+		scanf_s("%f", &nota3);
+
+		media = (nota1 + nota2 + nota3) / 3;
+
+		printf("A média aritmética deste aluno é: %f \n", media);
+	}
+}
+
+ #include <stdio.h>
+
+int main() {
+	int num, rep, i;
+	printf("Digite um número: ");
+	scanf_s("%d", &num);
+	printf("Digite a quantidade de repetições que deseja: ");
+	scanf_s("%d", &rep);
+
+	for (i = 0; i < rep; i++) {
+		num = num + 1;
+		printf("%d \n", num);
+	}
+		
+}
+
+#include <stdio.h>
+
+int main() {
+	int num, rep, i;
+	printf("Digite um número: ");
+	scanf_s("%d", &num);
+	printf("Digite a quantidade de repetições que deseja: ");
+	scanf_s("%d", &rep);
+
+	for (i = 0; i < rep; i++) {
+		num = num + 1;
+		printf("%d \n", num);
+	}
+
+}
+
+#include <stdio.h>
+
+int main() {
+	int num, i;
+
+	for (i = 0; i < 10; i++) {
+		printf("Digite um número: ");
+		scanf_s("%d", &num);
+		num = num * num;
+		printf("Seu quadrado é: %d \n", num);
+	}
+
+} 
+
+#include <stdio.h>
+
+int main() {
+	int i = 0;
+	do {
+		printf("Hello World! %d \n", i);
+		i = i + 1;
+	} while (i < 10);
+
+	i = 0;
+
+	while (i < 10) {
+		printf("Hello World! %d \n", i);
+		i = i + 1;
+	}
+
+	for (i = 0; i < 10; i++) {
+		printf("Hello World! %d \n", i);
+	}
+
+
+
+}
+*/
