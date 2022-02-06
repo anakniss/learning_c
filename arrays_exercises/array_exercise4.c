c68857959069882c760aa54a5cf84c65baa09a6f
Ana Kniss <ackniss@gmail.com>
2022-02-06 16:16:42 -0300

    Added exercise 4 of arrays

--- arrays_exercises/array_exercise4.c
+++ arrays_exercises/array_exercise4.c
@@ -1,4 +1,4 @@
-/*Fazer um algoritmo que leia vinte números inteiros armazenando-os em um vetor, a seguir, troque o
+/*4) Fazer um algoritmo que leia vinte números inteiros armazenando-os em um vetor, a seguir, troque o
 conteúdo da primeira posição do vetor com o conteúdo da décima primeira posição, o conteúdo da segunda
 posição com o da décima segunda, e assim, sucessivamente até trocar o conteúdo da décima com o da vigésima
 e escreva o vetor após estas modificações. */
