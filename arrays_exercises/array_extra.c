d69629de9217f23a879d83606256a6401fd4932f
Ana Kniss <ackniss@gmail.com>
2022-02-10 18:09:05 -0300

    Added extra exercise of arrays

---
+++ arrays_exercises/array_extra.c
@@ -0,0 +1,54 @@
+﻿/*Fazer um algoritmo que leia uma quantidade indeterminada de números até que sejam informados dez
+números distintos e escreva esses dez números.*/
+
+#include <stdio.h>
+
+int main() {
+	int nums[10];
+	int i;
+	int num = 0;
+	int i2 = 0;
+	int encontrei = 0;
+
+
+	printf("Digite dez números: \n"); 
+
+	for (i = 0; i < 10; i++){
+
+	scanf_s("%d", &nums[i]);
+	
+	}
+
+	i = 0;
+
+	printf("Digite os números: \n");
+
+	while (num != -1) {
+
+		scanf_s("%d", &num);
+
+		for (i = 0; i < 10; i++) {
+
+			if (nums[i] == num) {
+
+				encontrei = 1;
+
+			}
+		}
+
+		if (encontrei == 1){
+
+		printf("Esse número consta na lista\n");
+
+		}
+
+		else {
+
+		printf("Este número não consta na lista\n");
+
+		}
+
+		encontrei = 0;
+
+	}
+}
